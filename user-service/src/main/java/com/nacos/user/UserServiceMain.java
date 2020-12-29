d34299472ab351e8a308c1b654c067bb95f9faa7
dongyl <dongyuanlong@100tal.com>
2020-12-29 15:07:43 +0800

    add nacos discovery

--- user-service/src/main/java/com/nacos/user/UserServiceMain.java
+++ user-service/src/main/java/com/nacos/user/UserServiceMain.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import java.lang.invoke.MethodHandles;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceMain {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
